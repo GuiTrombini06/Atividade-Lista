371c29d2423723365842dc6d5b1342c5f10fb197
Guilherme Trombini de Castro <guicastroyt@gmail.com>
2026-05-15 21:09:42 -0300

    Add files via upload

---
+++ app07_SectionList_01/src/stylesheet/estilos1.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+export const estilos1 = StyleSheet.create({
+  fundo: {
+    flex: 1,
+    backgroundColor: '#f4f6f0',
+    paddingTop: 40,
+    paddingHorizontal: 10,
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+    color: '#2d3a1e',
+  },
+  cabecalho: {
+    backgroundColor: '#6b7c3b',
+    padding: 8,
+    borderRadius: 4,
+    marginTop: 4,
+  },
+  cabTexto: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#f4f6f0',
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#b5c490',
+  },
+  nome: {
+    fontSize: 16,
+    color: '#1a2410',
+  },
+});
